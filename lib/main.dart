e2015ba3a75733afca0a945cd41e85e39ccf482e
Travis-ugo <68594765+Travis-ugo@users.noreply.github.com>
2021-01-28 20:44:02 +0100

    mobile view

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:travis_ugo/Travis-ugo/utils/widgets.dart';
 import 'Travis-ugo/Contact/mobile_footer.dart';
-import 'Travis-ugo/Home/HomeDescktop/home_desktop.dart';
-import 'Travis-ugo/Home/HomeDescktop/menu_descktop.dart';
-import 'Travis-ugo/Project/project_mobile.dart';
+import 'Travis-ugo/Desktop/desktop_pro.dart';
+import 'Travis-ugo/Desktop/desktop_service.dart';
+import 'Travis-ugo/Desktop/desktpo_info.dart';
+import 'Travis-ugo/Desktop/menu_descktop.dart';
+import 'Travis-ugo/Dispatch/correction.dart';
+import 'Travis-ugo/Home/HomeMobile/mobile_view.dart';
+import 'Travis-ugo/Home/HomeMobile/project_mobile.dart';
+import 'Travis-ugo/Skills/Project/project_desktop.dart';
 import 'Travis-ugo/Skills/mobile_skills.dart';
-import 'Travis-ugo/ViewPort/didi_redesign.dart';
-import 'Travis-ugo/ViewPort/mobile_view.dart';
+import 'Travis-ugo/Utils/responsive.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,18 +24,16 @@ class MyApp extends StatelessWidget {
       title: 'Love Galour',
       debugShowCheckedModeBanner: false,
       routes: <String, WidgetBuilder>{
-        '/': (BuildContext context) => HomeMobileRedesign(),
+        '/': (BuildContext context) => Response(),
         '/mobileskills': (BuildContext context) => SKillsMobileRedesign(),
         '/mobileproject': (BuildContext context) => ProjectsMobileDesign(),
         '/mobileInfo': (BuildContext context) => MobileRiri(),
         '/mobilefooter': (BuildContext context) => MobileFooter(),
-        // '/': (BuildContext context) => MenuDesktop(),
-        // '/mobileproject': (BuildContext context) => ProjectDesktopRedesign(),
-        // '/mobileskills': (BuildContext context) => SkillDesktopRedesign(),
-        // '/mobileInfo': (BuildContext context) => DesktopRedesign(),
-        // '/mobilefooter': (BuildContext context) => MobileFooter(),
+        '/Desktopproject': (BuildContext context) => ProjectRiri(),
+        '/DesktopService': (BuildContext context) => ServiceDesktop(),
+        '/DesktopInfo': (BuildContext context) => DesktopRedesign(),
       },
-      // home: ProjectRiri(),
+      //home: Mobile(),
     );
   }
 }
